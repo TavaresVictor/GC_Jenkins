37dde0088f7e0301990d33a693e49394801e1d41
TavaresVictor <j.victortavares05@gmail.com>
2026-05-27 18:18:09 -0300

    observacoes

--- temperatura.py
+++ temperatura.py
@@ -1,5 +1,14 @@
+#Cenário 1 e 4
 def fahrenheit_para_celsius(f):
     return (f - 32) * 5/9
+
+#Cenario 2
+#def fahrenheit_para_celsius(f):
+#    return (f - 32) * 5/    Foraçar erro
+
+#Cenário 3
+#def fahrenheit_para_celsius(f):
+#    return (f - 30) * 5/9   Forçar instabilidade
 
 def celsius_para_fahrenheit(c):
     return (c * 9/5) + 32
